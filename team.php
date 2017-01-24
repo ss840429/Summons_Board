80f0d10321720624e3d17cb7bab20854e09e5cf9
Wlogsky <ss840429@gmail.com>
2017-01-25 00:29:20 +0800

    get checkbox value

--- team.php
+++ team.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset=utf-8>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv=X-UA-Compatible content="IE=edge">
     <meta name=viewport content="width=device-width, initial-scale=1">
     <meta name=description content="">
@@ -10,20 +10,17 @@
     <link rel=icon href=/Content/AssetsBS3/img/favicon.ico>
     <title>サモンズボード</title>
     <link href=https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css rel=stylesheet>
-    <link href=/Content/AssetsBS3/examples/justified-nav.css rel=stylesheet>
-    <!--[if lt IE 9]><script src=~/Scripts/AssetsBS3/ie8-responsive-file-warning.js></script><![endif]-->
-    <script src=/Scripts/AssetsBS3/ie-emulation-modes-warning.js></script>
-    <!--[if lt IE 9]><script src=https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js></script><script src=https://oss.maxcdn.com/respond/1.4.2/respond.min.js></script><![endif]-->
+
+    <script src="https://www.gstatic.com/firebasejs/3.5.2/firebase.js"></script>    <!-- FireBase -->
+    <script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+    <script src='https://cdn.firebase.com/js/client/2.1.1/firebase.js'></script>
+
     <style type="text/css">
     * {
         font-family: 'Microsoft JhengHei';
     }
     </style>
 </head>
-
-<?php
-header('Content-type: text/html; charset=utf-8');
-?>
 
 
 <body>
@@ -33,11 +30,12 @@ header('Content-type: text/html; charset=utf-8');
             <div role=navigation>
                 <ul class="nav nav-justified">
                     <li class=active><a href=home.php>Home</a></li>
-                        <li><a href=search.php>図鑑</a></li>
-                            <li><a href=team.php>Team</a></li>
-                                <li><a href=download.php>Downloads</a></li>
-                                    <li><a href=login.php>Manage</a></li>
-                                        <li><a href=about.php>About</a></li></ul>
+                    <li><a href=search.php>図鑑</a></li>
+                    <li><a href=team.php>Team</a></li>
+                    <li><a href=download.php>Downloads</a></li>
+                    <li><a href=login.php>Manage</a></li>
+                    <li><a href=about.php>About</a></li>
+                </ul>
             </div>
         </div>
         <div class=jumbotron style="background-image:url('pic/kanban3.jpg');background-repeat:no-repeat;background-size:100% 100%">
@@ -50,23 +48,23 @@ header('Content-type: text/html; charset=utf-8');
 			<div class=col-lg-6>
 				<h2>進階搜尋</h2>
 				<p>Type:</p>
-				<input type="checkbox" id="攻擊type" >攻擊 TYPE
-				<input type="checkbox" id="平衡type" >平衡 TYPE
-				<input type="checkbox" id="HP type" >HP TYPE
+				<input type="checkbox" value="攻擊type" name="Main_Type" >攻擊 TYPE
+				<input type="checkbox" value="平衡type" name="Main_Type">平衡 TYPE
+				<input type="checkbox" value="HP type" name="Main_Type">HP TYPE
 				<br>
-				<input type="checkbox" id="強襲type" >強襲 TYPE
-				<input type="checkbox" id="技能type" >技能TYPE
-				<input type="checkbox" id="反擊type" >反擊TYPE
-				<input type="checkbox" id="輔助type" >輔助TYPE
-				<input type="checkbox" id="防禦type" >防禦TYPE
+				<input type="checkbox" value="強襲type" name="Sec_Type">強襲 TYPE
+				<input type="checkbox" value="技能type" name="Sec_Type">技能TYPE
+				<input type="checkbox" value="反擊type" name="Sec_Type">反擊TYPE
+				<input type="checkbox" value="輔助type" name="Sec_Type">輔助TYPE
+				<input type="checkbox" value="防禦type" name="Sec_Type">防禦TYPE
 				<br>
 				<br>
 				<p>屬性:</p>
-				<input type="checkbox" id="Fire" >火屬
-				<input type="checkbox" id="Water" >水屬
-				<input type="checkbox" id="Tree" >木屬
-				<input type="checkbox" id="Light" >光屬
-				<input type="checkbox" id="Dark" >暗屬
+				<input type="checkbox" value="火" name="Type">火屬
+				<input type="checkbox" value="水" name="Type">水屬
+				<input type="checkbox" value="木" name="Type" checked>木屬
+				<input type="checkbox" value="光" name="Type" checked>光屬
+				<input type="checkbox" value="暗" name="Type">暗屬
 				<br>
 				<br>
 				<p>數值搜尋:</p>
@@ -83,7 +81,49 @@ header('Content-type: text/html; charset=utf-8');
 			</div>
             <div class=col-lg-6>
                 <h2>搜尋結果</h2><h5>點擊圖片可看詳細資料</h5>
-                <?php
+                <div id='datatable'></div>
+
+                <script>
+                    var db = new Firebase('https://summonsboard-2c5f0.firebaseio.com/') ;
+                    var result = []
+
+                    $('#submit').click( () => {
+                        // var p = new Promise( done => {
+                            // db.child('pet').orderByKey().once('value', snap => {
+
+                                // snap.forEach( e => {
+                                    
+                                    $('input:checkbox[name=Main_Type]:checked').each( (i, item) => { 
+                                        console.log($(item).val());
+                                    });
+
+                                    $('input:checkbox[name=Sec_Type]:checked').each( (i, item) => { 
+                                        console.log($(item).val());
+                                    });
+
+                                    $('input:checkbox[name=Type]:checked').each( (i, item) => { 
+                                        console.log($(item).val());
+                                    });
+
+                                    var hpLimit  = $('#HP').val();
+                                    var atkLimit = $('#ATK').val();
+
+                                    console.log(hpLimit, atkLimit);
+
+                                // });
+
+                            //     done();
+                            // }
+                        // });
+
+
+                    });
+
+
+
+                </script>   
+
+                <!-- <?php
                 if($_POST["action"] == "submit")
                 {
                     $sql_quest = "SELECT ID FROM pet WHERE 1";
@@ -131,8 +171,7 @@ header('Content-type: text/html; charset=utf-8');
                     }
 
                 }
-                
-                ?>
+                ?> -->
             </div>
         </div>
     </div>
