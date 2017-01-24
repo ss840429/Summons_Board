eae45b52b54f5d66566c18116da7118b1f5d53b6
Wlogsky <ss840429@gmail.com>
2017-01-25 02:37:06 +0800

    無用のものを捨てる

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset=utf-8>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv=X-UA-Compatible content="IE=edge">
     <meta name=viewport content="width=device-width, initial-scale=1">
     <meta name=description content="">
@@ -11,19 +11,9 @@
     <link rel=icon href=/Content/AssetsBS3/img/favicon.ico>
     <title>サモンズボード</title>
     <link href=https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css rel=stylesheet>
-    <link href=/Content/AssetsBS3/examples/justified-nav.css rel=stylesheet>
-    <!--[if lt IE 9]><script src=~/Scripts/AssetsBS3/ie8-responsive-file-warning.js></script><![endif]-->
-    <script src=/Scripts/AssetsBS3/ie-emulation-modes-warning.js></script>
-    <!--[if lt IE 9]><script src=https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js></script><script src=https://oss.maxcdn.com/respond/1.4.2/respond.min.js></script><![endif]-->
     <style type="text/css">
-    * {
-        font-family: 'Microsoft JhengHei';
-    }
+        * { font-family: 'Microsoft JhengHei'; }
     </style>
-
-<?php
-header('Content-type: text/html; charset=utf-8');
-?>
 
 
 </head>
@@ -78,7 +68,6 @@ header('Content-type: text/html; charset=utf-8');
 
         </div>
     </div>
-    <script src=/Scripts/AssetsBS3/ie10-viewport-bug-workaround.js></script>
     <footer class=footer>
          </br><p style = "text-align:center">2016&copy; Wlogsky、Patato、Ryanooo </p></footer>
 </body>
