c42ac2f8e26602bdb19128207b74069f87856462
Wlogsky <ss840429@gmail.com>
2017-01-25 20:39:51 +0800

    チームからのクエストに

--- search.php
+++ search.php
@@ -281,6 +281,24 @@
         $('#id').val(a);
         $('#submit').trigger('click');
     }
+
+    var strUrl = location.search;
+    var getPara, ParaVal;
+    var aryPara = [];
+
+    if (strUrl.indexOf("?") != -1) {
+        var getSearch = strUrl.split("?");
+        getPara = getSearch[1].split("&");
+        for (i = 0; i < getPara.length; i++) {
+            ParaVal = getPara[i].split("=");
+            aryPara.push(ParaVal[0]);
+            aryPara[ParaVal[0]] = ParaVal[1];
+        }
+        // console.log(aryPara['id']);
+    
+        TouchPic(aryPara['id'])
+    }
+
 </script>>
 
     <footer class=footer>
